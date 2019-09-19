48eb34698adfb10d29bbf1c9b850ab9d0aeff07d
Joao Pedro <joaopedrowatson@hotmail.com>
2019-09-19 13:55:20 -0300

    Primera tela finalizada

---
+++ app/src/main/java/br/com/etecia/apptask/MainActivity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.apptask;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
